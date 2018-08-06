e413ac9efcc1dec550d7353d435c9bcdab80817c
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-08-06 11:02:44 -0700

    Updated deploy script to be in npm run deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-aws s3 sync / s3://crypto-track.com --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-
-
-# aws s3 sync build/ s3://cryptotracker --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
